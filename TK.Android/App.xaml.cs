3fbc1c16b21e80dd055cce0a28a5721c2c63f487
Lightfire228 <lightfire228@gmail.com>
2025-08-25 22:27:12 -0400

    project init

---
+++ TK.Android/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace TK.Android;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ TK.Android/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace TK.Android;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
