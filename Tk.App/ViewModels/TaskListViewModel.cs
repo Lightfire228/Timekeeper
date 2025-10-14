db8b45fa39f89198401b715a07746dc4e96e9de8
Lightfire228 <lightfire228@gmail.com>
2025-10-13 21:36:25 -0400

    delete maui app

--- Tk.App/ViewModels/TaskListViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Tk.App.ViewModels;
-
-public class TaskListViewModel {
-
-    public required string Name     { get; set; }
-    public required string DueDate  { get; set; }
-    public required bool   Complete { get; set; }
-    
-}
