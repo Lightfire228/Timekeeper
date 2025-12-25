3f3631ad68ede024944cc4f2cbfe6b5c44e30f09
Lightfire228 <lightfire228@gmail.com>
2025-12-24 22:08:44 -0500

    re init svelte

---
+++ tk_ui/src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+    import * as api from "$lib/api.ts";
+
+    let tasks = $state([]);
+
+    api.getTasks().then(res => tasks = res).catch(err => console.log(`err: {err}`));
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+
